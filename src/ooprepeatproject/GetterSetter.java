5c1ac7fe1f6217231741f1b491cb72a408e2db43
james <james@LAPTOP-CHRQ3ML1.Home>
2017-09-27 19:48:13 +0100

    new commit

---
+++ src/ooprepeatproject/GetterSetter.java
@@ -0,0 +1,28 @@
+package ooprepeatproject;
+
+/**
+ * @author x16114388
+ */
+
+public class GetterSetter {
+
+    public int scoreMain = 0;
+    public int counter = 1;
+ 
+    
+    public int getScore() {
+        return scoreMain;
+    }
+
+    public void setScore(int score) {
+        this.scoreMain = score;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
---
+++ src/ooprepeatproject/OopRepeatProject.java
@@ -0,0 +1,18 @@
+package ooprepeatproject;
+
+/**
+ *
+ * @author x16114388
+ */
+public class OopRepeatProject {
+
+    public static void main(String[] args) {
+
+       
+Questions qGUI = new Questions();
+qGUI.setVisible(true);
+qGUI.setLocationRelativeTo(null);
+       
+    }
+
+}
